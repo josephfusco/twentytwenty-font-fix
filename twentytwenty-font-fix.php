8d3cb5b5120b853fa7660fa636ffc6f5911a11bc
Joseph Fusco <jfusco@masonite.com>
2020-01-20 08:36:35 -0500

    Initial commit
    Signed-off-by: Joseph Fusco <jfusco@masonite.com>

---
+++ twentytwenty-font-fix.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Plugin Name:       Twenty Twenty Font Fix
+ * Description:       Fixes a font issue within the default Twenty Twenty theme that appears in Chrome on macOS.
+ * Version:           1.0.0
+ * Author:            Joseph Fusco
+ * Author URI:        https://josephfus.co
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+namespace TwentyTwentyFontFix;
+
+// If file is called directly, abort.
+defined( 'WPINC' ) || die();
+
+/**
+ * Embed CSS into the document head containing fix.
+ */
+function embed_styles() : void {
+    $stylesheet = get_stylesheet();
+
+    // Exit if current theme is not Twenty Twenty.
+    if ( 'twentytwenty' !== $stylesheet ) {
+        return;
+    }
+
+    remove_editor_styles();
+
+    ?>
+    <style>
+        .entry-content p,
+        .entry-content ol,
+        .entry-content ul,
+        .entry-content dl,
+        .entry-content dt,
+        .widget_text p,
+        .widget_text ol,
+        .widget_text ul,
+        .widget_text dl,
+        .widget_text dt,
+        .widget-content .rssSummary,
+        body#tinymce.wp-editor.content p,
+        body#tinymce.wp-editor.content ol,
+        body#tinymce.wp-editor.content ul,
+        body#tinymce.wp-editor.content dl,
+        body#tinymce.wp-editor.content dt {
+            font-family: Garamond, "Times New Roman", serif !important;
+        }
+    </style>
+    <?php
+}
+add_action( 'admin_head', __NAMESPACE__ . '\embed_styles' );
+add_action( 'wp_head', __NAMESPACE__ . '\embed_styles' );
